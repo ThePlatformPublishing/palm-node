659054751b1952da6a24537f36dce8aba8291b95
sergiorivera50 <sriveralopez50@gmail.com>
2022-05-23 18:40:01 +0100

    Mount local ssh directory to share keys

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 echo "Make sure to add your AWS credentials into .env"
-docker build -t ansible-terraform . && docker run -it --env-file .env -v $(pwd):/palm-node ansible-terraform
+docker build -t ansible-terraform . && docker run -it --env-file .env -v ~/.ssh:/root/.ssh -v $(pwd):/palm-node ansible-terraform
