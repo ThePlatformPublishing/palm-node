3dcb35ae36ce371887c0010e0bc9fa4d4bd1753f
sergiorivera50 <sriveralopez50@gmail.com>
2022-05-23 18:28:28 +0100

    Added AWS creds in .env

--- docker.sh
+++ docker.sh
@@ -1,1 +1,2 @@
-docker build -t ansible-terraform . && docker run -it -v $(pwd):/palm-node ansible-terraform
+echo "Make sure to add your AWS credentials into .env"
+docker build -t ansible-terraform . && docker run -it --env-file .env -v $(pwd):/palm-node ansible-terraform
